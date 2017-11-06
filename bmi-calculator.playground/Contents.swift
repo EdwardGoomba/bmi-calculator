07e9e37a0cd424287af8a10a8b63e948d32515d3
EdwardGoomba <ask@edwardvdesign.com>
2017-11-06 16:46:56 -0500

    Set up bmi calc function

--- bmi-calculator.playground/Contents.swift
+++ bmi-calculator.playground/Contents.swift
@@ -1,5 +1,26 @@
-//: Playground - noun: a place where people can play
+//BMI Calculator - inputs weight and height and returns BMI
 
 import UIKit
 
-var str = "Hello, playground"
+func calcBMI (yourWeight : Int, yourHeight : Int) -> Int {
+    
+    let bmi = (yourHeight / (yourHeight * yourHeight))
+    
+    if bmi > 25 {
+        return "Your BMI is: \(bmi). You are overweight..."
+        
+    }
+    
+    else if bmi > 18.5 && bmi <= 25 {
+        print("You are of normal weight")
+    }
+    
+    else {
+        print("You are underweight")
+    }
+    
+    return bmi
+    
+}
+
+calcBmi
